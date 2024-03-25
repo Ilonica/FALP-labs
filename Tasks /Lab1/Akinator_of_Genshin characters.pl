376dbd1a56f3ff6313ea2562f0f55cbe8a822f0a
Ilonica <95592035+Ilonica@users.noreply.github.com>
2024-03-25 03:02:43 +0300

    Задание 1.6 - изменена структура акинатора, указана унификация переменных

--- Tasks /Lab1/Akinator_of_Genshin characters.pl
+++ Tasks /Lab1/Akinator_of_Genshin characters.pl
@@ -1,133 +1,65 @@
+:- dynamic asked/2.
+
+% Основной предикат.
 main :-
     retractall(asked(_,_)),
-    fault(Problem),
-    !,
+    findall(Question, question(_, Question), Questions),
+    maplist(request, Questions),
+    find_character(Character),
     nl,
-    write('Скорее всего вы загадали '), write(Problem), write(.), nl.
+    write('Скорее всего вы загадали '), write(Character), write('.'), nl.
 main :-
     nl,
     write('Система не смогла угадать персонажа.'), nl.
 
-problem(hair) :-
-    query('У вашего персонажа темные волосы?').
+% Предикат, который представляет список вопросов для акинатора и
+% переменные для их идентификации.
+question(hair, 'У вашего персонажа темные волосы?').
+question(eyes, 'У вашего персонажа светлые глаза?').
+question(age, 'Ваш персонаж взрослый?').
+question(gender, 'Ваш персонаж женщина?').
+question(arhont, 'Ваш персонаж Архонт?').
+question(fatui, 'Ваш персонаж предвестник Фатуи?').
 
-problem(eyes) :-
-    query('У вашего персонажа светлые глаза?').
+% Предикат, с именами персонажей и их характеристиками.
+character(фурину, [hair-n, eyes-y, age-y, gender-y, arhont-y, fatui-n]).
+character(райдэн_эи, [hair-n, eyes-n, age-y, gender-y, arhont-y, fatui-n]).
+character(чжун_ли, [hair-y, eyes-y, age-y, gender-n, arhont-y, fatui-n]).
+character(cандоре, [hair-n, eyes-y, age-y, gender-y, arhont-n, fatui-y]).
+character(cару, [hair-y, eyes-y, age-y, gender-y, arhont-n, fatui-n]).
+character(колумбину, [hair-y, eyes-n, age-y, gender-y, arhont-n, fatui-y]).
+character(венти, [hair-y, eyes-n, age-y, gender-n, arhont-y, fatui-n]).
+character(тарталью, [hair-n, eyes-y, age-y, gender-n, arhont-n, fatui-y]).
+character(панталоне, [hair-y, eyes-n, age-y, gender-n, arhont-n, fatui-y]).
+character(арлекину, [hair-n, eyes-n, age-y, gender-y, arhont-n, fatui-y]).
+character(эмбер, [hair-y, eyes-n, age-y, gender-y, arhont-n, fatui-n]).
+character(ёимию, [hair-n, eyes-y, age-y, gender-y, arhont-n, fatui-n]).
+character(нахиду, [hair-n, eyes-y, age-n, gender-y, arhont-y, fatui-n]).
+character(кэйю, [hair-y, eyes-y, age-y, gender-n, arhont-n, fatui-n]).
+character(странника, [hair-y, eyes-n, age-y, gender-n, arhont-n, fatui-n]).
+character(дилюка, [hair-n, eyes-y, age-y, gender-n, arhont-n, fatui-n]).
+character(дотторе, [hair-n, eyes-n, age-y, gender-n, arhont-n, fatui-y]).
+character(кокоми, [hair-n, eyes-n, age-y, gender-y, arhont-n, fatui-n]).
+character(паймон, [hair-n, eyes-n, age-n, gender-y, arhont-n, fatui-n]).
+character(син_цю, [hair-n, eyes-y, age-n, gender-n, arhont-n, fatui-n]).
 
-problem(age) :-
-    query('Ваш персонаж взрослый?').
+% Предикат, который задает вопрос пользователю и записывает ответы.
+% request(+Question)
+request(Question) :-
+    write(Question), write(' (y/n)? '),
+    read(Reply),
+    assert(asked(Question, Reply)).
 
-problem(gender) :-
-    query('Ваш персонаж женщина?').
+% Предикаты для определения персонажа на основе ответов пользователя.
+% find_character(+Character)
+find_character(Character) :-
+    character(Character, Traits),
+    check_traits(Traits).
 
-problem(arhont) :-
-    query('Ваш персонаж Архонт?').
-
-problem(fatui) :-
-    query('Ваш персонаж предвестник Фатуи?').
-
-fault(фурину) :-
-    problem(arhont),
-    problem(age),
-    problem(gender),
-    problem(eyes).
-
-fault(райдэн_эи) :-
-    problem(arhont),
-    problem(age),
-    problem(gender),
-    problem(hair).
-
-fault(чжун_ли) :-
-    problem(arhont),
-    problem(age),
-    problem(eyes),
-    problem(hair).
-
-fault(сандоре) :-
-    problem(fatui),
-    problem(gender),
-    problem(age),
-    problem(eyes).
-
-fault(сару) :-
-    problem(gender),
-    problem(age),
-    problem(eyes),
-    problem(hair).
-
-fault(колумбину) :-
-    problem(fatui),
-    problem(gender),
-    problem(age),
-    problem(hair).
-
-fault(венти) :-
-    problem(arhont),
-    problem(age),
-    problem(hair).
-
-fault(тарталью) :-
-    problem(fatui),
-    problem(age),
-    problem(eyes).
-
-fault(панталоне) :-
-    problem(fatui),
-    problem(age),
-    problem(hair).
-
-fault(арлекину) :-
-    problem(fatui),
-    problem(gender),
-    problem(age).
-
-fault(эмбер) :-
-    problem(gender),
-    problem(age),
-    problem(hair).
-
-fault(ёимию) :-
-    problem(gender),
-    problem(age),
-    problem(eyes).
-
-fault(нахиду) :-
-    problem(arhont),
-    problem(eyes),
-    problem(gender).
-
-fault(кэйю) :-
-    problem(age),
-    problem(eyes),
-    problem(hair).
-
-fault(странника) :-
-    problem(age),
-    problem(hair).
-
-fault(дилюка) :-
-    problem(age),
-    problem(eyes).
-
-fault(дотторе) :-
-    problem(fatui),
-    problem(age).
-
-fault(кокоми) :-
-    problem(gender),
-    problem(age).
-
-fault(паймон) :-
-    problem(gender).
-
-fault(син_цю) :-
-    problem(eyes).
-
-query(Prompt) :-
-    (   asked(Prompt, Reply) -> true
-    ;   nl, write(Prompt), write(' (y/n)? '),
-        read(X),(X = y -> Reply = y ; Reply = n),
-	assert(asked(Prompt, Reply))
-    ),
-    Reply = y.
+check_traits([]).
+% check_traits([+Trait|-T])
+check_traits([Trait|T]) :-
+    question(Fact, Question),
+    Trait = Fact-Answer,
+    asked(Question, Answer),
+    check_traits(T).
