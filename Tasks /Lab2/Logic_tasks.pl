f282d520085ded629190e363749fe6c5a4c392d2
Ilonica <95592035+Ilonica@users.noreply.github.com>
2024-05-18 05:02:59 +0300

    Задание 2.8 - решение логической задачи про братьев

--- Tasks /Lab2/Logic_tasks.pl
+++ Tasks /Lab2/Logic_tasks.pl
@@ -47,3 +47,61 @@ solve_problem:-
     is_in_list(Containers, [Vessel3, milk]),
     is_in_list(Containers, [Vessel4, kvass]),
     write('Containers: '), write(Containers), nl.
+
+% Задание 8
+% Один из пяти братьев разбил окно.
+% Андрей сказал: Это или Витя, или Коля.
+% Витя сказал: Это сделал не я и не Юра.
+% Дима сказал: Нет, один из них сказал правду, а другой неправду.
+% Юра сказал: Нет, Дима ты не прав.
+% Их отец, которому, конечно можно доверять, уверен, что не менее трех братьев
+% сказали правду. Кто разбил окно?
+
+% Основной предикат
+solve_guilt:-
+        Brothers=[_,_,_,_,_],
+
+        % Предположение, что Юра лжёт
+        % Андрей сказал: Это или Витя, или Коля
+        (is_in_list(Brothers, [kolya, guilty]); is_in_list(Brothers, [vitya, guilty])),
+        is_in_list(Brothers, [andrey, innocent]),
+        is_in_list(Brothers, [yura, innocent]),
+        is_in_list(Brothers, [dima, innocent]),
+
+        % Витя сказал: Это сделал не я и не Юра
+        is_in_list(Brothers, [yura, innocent]),
+        is_in_list(Brothers, [vitya, innocent]),
+        (is_in_list(Brothers, [andrey, guilty]); is_in_list(Brothers, [kolya, guilty]); is_in_list(Brothers, [dima, guilty])),
+
+        % Дима сказал: Нет, один из них сказал правду, а другой неправду
+        (is_in_list(Brothers, [kolya, guilty]); is_in_list(Brothers, [vitya, guilty])),
+        is_in_list(Brothers, [andrey, innocent]),
+        is_in_list(Brothers, [yura, innocent]),
+        is_in_list(Brothers, [dima, innocent]),
+
+        % Предположение, что Дима лжёт
+        % Андрей сказал: Это или Витя, или Коля
+        (is_in_list(Brothers, [kolya, guilty]); is_in_list(Brothers, [vitya, guilty])),
+        is_in_list(Brothers, [andrey, innocent]),
+        is_in_list(Brothers, [yura, innocent]),
+        is_in_list(Brothers, [dima, innocent]),
+
+        % Витя сказал: Это сделал не я и не Юра
+        is_in_list(Brothers, [yura, innocent]),
+        is_in_list(Brothers, [vitya, innocent]),
+        (is_in_list(Brothers, [andrey, guilty]); is_in_list(Brothers, [kolya, guilty]); is_in_list(Brothers, [dima, guilty])),
+
+        % Возможно лжёт Андрей
+        (is_in_list(Brothers, [kolya, guilty]); is_in_list(Brothers, [vitya, guilty])),
+        is_in_list(Brothers, [andrey, innocent]),
+        is_in_list(Brothers, [yura, innocent]),
+        is_in_list(Brothers, [dima, innocent]),
+
+        % Возможно лжёт Андрей
+        is_in_list(Brothers, [yura, innocent]),
+        is_in_list(Brothers, [vitya, innocent]),
+        (is_in_list(Brothers, [andrey, guilty]); is_in_list(Brothers, [kolya, guilty]); is_in_list(Brothers, [dima, guilty])),
+
+        % Выводим состав группы
+        write('Brothers: '), write(Brothers), nl.
+
